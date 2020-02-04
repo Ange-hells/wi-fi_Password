64d19923bbd40e0fe0b52b0e3fb627ed660fcf2c
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-04 17:00:04 +0000

    creat page1

---
+++ src/Controller/Index.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class Index extends AbstractController
+{
+    public function index()
+    {
+        $user_first_name = "hello";
+        $userNotification = ['...', '...'];
+        
+        return $this->render('Index.html.twig', [
+            'user_first_name' => $user_first_name,
+            'notification' => $userNotification
+        ]);
+    }
+}
