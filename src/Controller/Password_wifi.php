074b8e5cd7d9aa215ec9929a48c77c38fe312b3f
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-07 08:43:55 +0000

    Update Password_wifi.php

--- src/Controller/Password_wifi.php
+++ src/Controller/Password_wifi.php
@@ -5,14 +5,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class Password_wifi extends AbstractController
 {
-    public function index()
+    public function mdp()
     {
-        $user_first_name = "hello";
+        $password = Password_wifi::getMdp(1);
         $userNotification = ['...', '...'];
         
-        return $this->render('Index.html.twig', [
-            'user_first_name' => $user_first_name,
+        return $this->render('mdp.html.twig', [
+            'password' => $password,
             'notification' => $userNotification
         ]);
+    }
+
+    public function getMdp($durer)
+    {
+        if $durer == 1{
+            $password = "salut"
+        }elseif $durer == 2{
+            $password = "hello"
+        }else {
+            $password = "hi"
+        };
+        return $password;
     }
 }
