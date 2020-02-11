d0c8fcdee3a4e02cd266ea54d0b4a2210ed08c4f
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-11 16:59:14 +0000

    Update Index.php

--- src/Controller/Index.php
+++ src/Controller/Index.php
@@ -7,12 +7,55 @@ class Index extends AbstractController
 {
     public function index()
     {
-        $user_first_name = "hello";
-        $userNotification = ['...', '...'];
+        // $user_first_name = "hello";
         
         return $this->render('Index.html.twig', [
-            'user_first_name' => $user_first_name,
-            'notification' => $userNotification
+            // 'user_first_name' => $user_first_name,
         ]);
     }
+
+    public function mdp($temp)
+    {
+        $password = $this->getMdp($temp);
+        
+        return $this->render('mdp.html.twig', [
+            'password' => $password,
+        ]);
+    }
+
+    public function getMdp($durer)
+    {
+        if ($durer == 1){
+            $password = "salut";
+        }elseif ($durer == 2){
+            $password = "hello";
+        }else{
+            $password = "hi";
+        }
+        return $password;
+    }
+
+
+    public function getMdp1h()
+    {
+        //get un mdp de 1h
+        //dellete this mdp de 1h
+        return $password;
+    }
+
+    public function getMdp2h()
+    {
+        //get un mdp de 2h
+        //dellete this mdp de 2h
+        return $password;
+    }
+
+    public function getMdp3h()
+    {
+        //get un mdp de 3h
+        //dellete this mdp de 3h
+        return $password;
+    }
+
+
 }
